9f4cd67c9b508cb3b535b04c9a4818bddace3ae6
B-Nilson <nilson.brayden@gmail.com>
2026-02-18 15:46:21 -0800

    cleanup/clarify primary tab functions

--- js/page_load.js
+++ js/page_load.js
@@ -1,19 +1,19 @@
 function on_page_load(header_class, icons) {
     set_reactable_search_placeholder("search...");
-    insert_dropdown_icons(icons);
+    insert_primary_tab_icons(icons);
     //hide_loading_screen(header_class); // TODO: remove if not needed for other reports
 };
 
-function insert_dropdown_icons(icons) {
-    let dropdown_class = ".primary-tabset";
-    for (var i = 1; i <= icons.length; i++) {
-        let icon = make_dropdown_icons(icons[i - 1]);
-        let dropdown_entry = $(dropdown_class).parentElement.children[0].children[i - 1].children[0];
-        dropdown_entry.prepend(icon);
+function insert_primary_tab_icons(icons) {
+    let p_tabs_class = ".primary-tabset";
+    for (var i = 0; i < icons.length; i++) {
+        let icon = make_primary_tab_icons(icons[i]);
+        let tab_i = $(p_tabs_class).children().first().children()[i].childNodes[0];
+        tab_i.prepend(icon);
     }
 }
 
-function make_dropdown_icons(icon_path) {
+function make_primary_tab_icons(icon_path) {
     var icon = document.createElement("img");
     icon.setAttribute("src", icon_path);
     icon.style.width = "40px";
