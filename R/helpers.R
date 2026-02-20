bbf97b8daa162cbfe5f898bdb23011c29f050045
B-Nilson <nilson.brayden@gmail.com>
2026-02-20 10:52:24 -0800

    abstract build_tabs() helper

--- R/helpers.R
+++ R/helpers.R
@@ -15,3 +15,24 @@ make_summary_chunk <- function(contents) {
 ::::'
   template |> sprintf(contents)
 }
+
+build_tabs <- function(
+  tab_names,
+  plot_paths,
+  plot_captions,
+  report_dir = "./"
+) {
+  chunks <- tab_names |>
+    sapply(\(tab_name) {
+      card <- plot_paths[[tab_name]] |>
+        stringr::str_replace(stringr::fixed(report_dir), "./") |>
+        plot_card(plot_captions[[tab_name]])
+      "## %s\n%s" |> sprintf(tab_name, card)
+    })
+
+  ":::::::: panel-tabset
+%s
+::::::::" |>
+    sprintf(paste(chunks, collapse = "\n")) |>
+    knitr::asis_output()
+}
