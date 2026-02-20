6dccf0f278a4b7b0aaa22bfc75c6d1d857821f9e
B-Nilson <nilson.brayden@gmail.com>
2026-02-20 11:52:46 -0800

    implement build_tabs, adding ability to include a table and iframes, save map tables to .html, and split donuts into mean/max tabs

--- R/helpers.R
+++ R/helpers.R
@@ -20,15 +20,32 @@ build_tabs <- function(
   tab_names,
   plot_paths,
   plot_captions,
-  report_dir = "./"
+  report_dir = "./",
+  table_buttons = NULL,
+  tables = NULL,
+  iframe = FALSE
 ) {
   chunks <- tab_names |>
     sapply(\(tab_name) {
       card <- plot_paths[[tab_name]] |>
         stringr::str_replace(stringr::fixed(report_dir), "./") |>
-        plot_card(plot_captions[[tab_name]])
+        plot_card(plot_captions[[tab_name]], iframe = iframe)
       "## %s\n%s" |> sprintf(tab_name, card)
     })
+
+  if (!is.null(tables)) {
+    chunks <- tab_names |>
+      lapply(\(tab_name) {
+        "%s\n%s\n%s" |>
+          sprintf(
+            chunks[[names(tab_names[tab_names == tab_name])]],
+            table_buttons[[tab_name]],
+            tables[[tab_name]] |>
+              stringr::str_replace(stringr::fixed(report_dir), "./") |>
+              plot_card("", iframe = TRUE)
+          )
+      })
+  }
 
   ":::::::: panel-tabset
 %s
