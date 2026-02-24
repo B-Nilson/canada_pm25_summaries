d099e395b31e54c440e35a965637689cdf148d3e
B-Nilson <nilson.brayden@gmail.com>
2026-02-24 08:28:57 -0800

    remove <detail> tags around auto summaries

--- R/helpers.R
+++ R/helpers.R
@@ -1,12 +1,7 @@
 make_summary_chunk <- function(contents) {
   template <- ':::: summary-card
 
-<details>
-<summary>Click for an automated text summary.</summary>
-
 %s
-  
-</details>
 
 ::::'
   template |> sprintf(contents)
