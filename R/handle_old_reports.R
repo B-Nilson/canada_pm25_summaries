342fdcf17f6939a66aabfe0ffd9253bb55a56928
B-Nilson <nilson.brayden@gmail.com>
2026-02-24 19:58:12 -0800

    Fix: #87 by using post-render code to copy index files over to historic/ instead of doing so within the report front matter

--- R/handle_old_reports.R
+++ R/handle_old_reports.R
@@ -1,10 +1,7 @@
 handle_old_reports <- function(
   report_dir,
   historic_dir = "historic",
-  data_dir = "data",
-  figure_dir = "plots",
   report_pattern = "\\d\\.html$",
-  max_date,
   type = c("daily", "monthly", "seasonal")[1]
 ) {
   index_path <- report_dir |> file.path("index.html")
@@ -17,52 +14,6 @@ handle_old_reports <- function(
     names(existing_reports) <- existing_reports |>
       basename() |>
       stringr::str_remove("\\.html")
-  }
-
-  # Rename index.html and move plots/data if not done so already
-  if (index_exists) {
-    previous_report <- max_date |>
-      get_previous_report_name(
-        type = type,
-        months_in_seasons = months_in_seasons
-      )
-
-    historic_dir <- report_dir |> file.path(historic_dir)
-    need_to_rename <- (length(existing_reports) &
-      !previous_report %in% names(existing_reports)) |
-      length(existing_reports) == 0
-    if (need_to_rename) {
-      new_path <- historic_dir |>
-        file.path(paste0(previous_report, ".html"))
-      index_path |> file.copy(new_path)
-      existing_reports <- existing_reports |> c(new_path)
-      names(existing_reports)[length(existing_reports)] <- previous_report
-
-      # Copy libs
-      historic_dir |>
-        file.path("index_files") |>
-        dir.create(showWarnings = FALSE)
-      report_dir |>
-        file.path("index_files") |>
-        file.copy(
-          to = historic_dir,
-          recursive = TRUE
-        )
-
-      # Copy data and plots
-      dir.create(historic_dir |> file.path(data_dir), showWarnings = FALSE)
-      report_dir |>
-        file.path(data_dir) |>
-        file.copy(to = historic_dir, recursive = TRUE)
-      dir.create(historic_dir |> file.path(figure_dir), showWarnings = FALSE)
-      report_dir |>
-        file.path(figure_dir) |>
-        file.copy(to = historic_dir, recursive = TRUE)
-    } else {
-      "Either no historic reports, or the report for %s already exists - not renaming index.html." |>
-        sprintf(previous_report) |>
-        warning()
-    }
   }
 
   existing_report_display_names <- existing_reports |>
--- R/post_render.R
+++ R/post_render.R
@@ -23,8 +23,8 @@ fix_no_date_citations <- function(report_dirs, index_name = "index.html") {
       )
 
     # Replace n.d. with modified date, {RNAME} with report name
-    date_line_number <- report_lines |> stringr::str_which("<div id=\"ref-") + 1
-    report_lines[date_line_number] <- report_lines[date_line_number] |>
+    date_line <- report_lines |> stringr::str_which("<div id=\"ref-") + 1
+    report_lines[date_line] <- report_lines[date_line] |>
       stringr::str_replace(
         pattern = stringr::fixed("n.d."),
         replacement = modified_date |> paste0(".")
@@ -35,14 +35,16 @@ fix_no_date_citations <- function(report_dirs, index_name = "index.html") {
       )
 
     # Replace {RNAME} with report name if on different line
-    rname_line_number <- report_lines |>
+    rname_line <- report_lines |>
       stringr::str_which(stringr::fixed("{RNAME}"))
-    if (date_line_number != rname_line_number) {
-      report_lines[rname_line_number] <- report_lines[rname_line_number] |>
-        stringr::str_replace(
-          pattern = stringr::fixed("{RNAME}"),
-          replacement = report_name
-        )
+    if (length(rname_line) > 0) {
+      if (date_line != rname_line) {
+        report_lines[rname_line] <- report_lines[rname_line] |>
+          stringr::str_replace(
+            pattern = stringr::fixed("{RNAME}"),
+            replacement = report_name
+          )
+      }
     }
 
     # Overwrite file
@@ -50,6 +52,55 @@ fix_no_date_citations <- function(report_dirs, index_name = "index.html") {
   }
 }
 
-# Run for each report type
+copy_index_to_historic <- function(
+  report_dir,
+  to_transfer = c("index.html", "index_files", "plots", "data")
+) {
+  source("R/report_name_codec.R")
+  inputs <- report_dir |> file.path(to_transfer)
+  if (!file.exists(inputs[1])) {
+    warning("No index.html found in ", report_dir, "/")
+    return(invisible())
+  }
+
+  # Extract report name from title in file and convert to file name
+  report_file_name <- readLines(inputs[1]) |>
+    suppressWarnings() |>
+    stringr::str_subset("<h1 class=\"title\">") |>
+    stringr::str_extract(
+      "<mark class=\"bg-info\">(.*)</mark></h1>",
+      group = 1
+    ) |>
+    get_report_end_dates() |>
+    get_report_file_names(type = report_dir) |>
+    paste0(".html")
+
+  outputs <- report_dir |>
+    file.path(
+      "historic",
+      to_transfer |>
+        dplyr::recode_values("index.html" ~ report_file_name, default = "/")
+    )
+  for (i in seq_along(inputs)) {
+    is_file <- file.exists(inputs[i]) & !dir.exists(inputs[i])
+    if (!(dir.exists(inputs[i]) | is_file)) {
+      next
+    }
+    file.copy(
+      from = inputs[i],
+      to = outputs[i],
+      recursive = !is_file,
+      overwrite = TRUE
+    )
+  }
+  invisible()
+}
+
+# Fix n.d. and {RNAME} in citations for each report type
 report_dirs <- c("daily", "monthly", "seasonal")
 fix_no_date_citations(report_dirs)
+
+# Copy index and related files over to historic dir
+for (report_dir in report_dirs) {
+  copy_index_to_historic(report_dir)
+}
