ab4cae0c519700d9482fa7ec0bc071f5e5f246e5
B-Nilson <nilson.brayden@gmail.com>
2026-02-24 20:22:49 -0800

    use historic url in citation

--- R/post_render.R
+++ R/post_render.R
@@ -1,3 +1,4 @@
+source("R/report_name_codec.R")
 fix_no_date_citations <- function(report_dirs, index_name = "index.html") {
   reports <- report_dirs |> file.path(index_name)
   reports <- reports[file.exists(reports)]
@@ -13,6 +14,9 @@ fix_no_date_citations <- function(report_dirs, index_name = "index.html") {
         "<mark class=\"bg-info\">(.*)</mark></h1>",
         group = 1
       )
+    report_file_name <- report_name |>
+      get_report_end_dates() |>
+      get_report_file_names(type = dirname(report))
 
     # Extract modified date from header
     modified_date <- report_lines |>
@@ -22,29 +26,32 @@ fix_no_date_citations <- function(report_dirs, index_name = "index.html") {
         group = 1
       )
 
-    # Replace n.d. with modified date, {RNAME} with report name
+    # Replace n.d. with modified date, {RNAME} with report name, {RFILE} with report file name
     date_line <- report_lines |> stringr::str_which("<div id=\"ref-") + 1
-    report_lines[date_line] <- report_lines[date_line] |>
-      stringr::str_replace(
-        pattern = stringr::fixed("n.d."),
-        replacement = modified_date |> paste0(".")
-      ) |>
-      stringr::str_replace(
-        pattern = stringr::fixed("{RNAME}"),
-        replacement = report_name
-      )
-
-    # Replace {RNAME} with report name if on different line
+    if (length(date_line) > 0) {
+      report_lines[date_line] <- report_lines[date_line] |>
+        stringr::str_replace(
+          pattern = stringr::fixed("n.d."),
+          replacement = modified_date |> paste0(".")
+        )
+    }
+    rfile_line <- report_lines |>
+      stringr::str_which(stringr::fixed("{RFILE}"))
+    if (length(rfile_line) > 0) {
+      report_lines[rfile_line] <- report_lines[rfile_line] |>
+        stringr::str_replace_all(
+          pattern = stringr::fixed("{RFILE}"),
+          replacement = report_file_name
+        )
+    }
     rname_line <- report_lines |>
       stringr::str_which(stringr::fixed("{RNAME}"))
     if (length(rname_line) > 0) {
-      if (date_line != rname_line) {
-        report_lines[rname_line] <- report_lines[rname_line] |>
-          stringr::str_replace(
-            pattern = stringr::fixed("{RNAME}"),
-            replacement = report_name
-          )
-      }
+      report_lines[rname_line] <- report_lines[rname_line] |>
+        stringr::str_replace_all(
+          pattern = stringr::fixed("{RNAME}"),
+          replacement = report_name
+        )
     }
 
     # Overwrite file
@@ -56,7 +63,6 @@ copy_index_to_historic <- function(
   report_dir,
   to_transfer = c("index.html", "index_files", "plots", "data")
 ) {
-  source("R/report_name_codec.R")
   inputs <- report_dir |> file.path(to_transfer)
   if (!file.exists(inputs[1])) {
     warning("No index.html found in ", report_dir, "/")
