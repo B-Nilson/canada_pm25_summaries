8979dbab8188472d10e9df57af64711ffe133c3e
Brayden Nilson <nilson.brayden@gmail.com>
2026-03-04 09:51:33 -0800

    add (adhoc) test for setting params (to help with testing report chunks manually)

---
+++ tests/test-params.R
@@ -0,0 +1,19 @@
+# able to set daily params
+{
+  setwd("daily")
+  on.exit(setwd("../"))
+
+  # Read yaml header and parse params
+  daily_lines <- "index.qmd" |> readLines(n = 100)
+  yaml_start_end <- which(daily_lines == "---")
+  daily_yaml <- daily_lines[(yaml_start_end[1] + 1):(yaml_start_end[2] - 1)]
+  params <- yaml::read_yaml(text = daily_yaml, eval.expr = TRUE)$params
+
+  # Handle "value" key for multi-key params
+  for (param_name in names(params)) {
+    param <- params[[param_name]]
+    if ("value" %in% names(param)) {
+      params[[param_name]] <- param$value
+    }
+  }
+}
