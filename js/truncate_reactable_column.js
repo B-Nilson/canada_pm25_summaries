f32fbb49db89261cae9d47ea631d0b4fea445be3
Brayden Nilson <nilson.brayden@gmail.com>
2026-04-02 13:16:34 -0700

    refactor reactable column truncation so it applies to the raw data and is not visible

--- js/truncate_reactable_column.js
+++ js/truncate_reactable_column.js
@@ -1,34 +1,22 @@
-function add_truncate_listeners(columns) {
+function add_truncate_listeners(keys) {
     let all_reactables = document.getElementsByClassName("reactable");
     Array.from(all_reactables).forEach(
-        elem => Reactable.onStateChange(elem.id, state => truncate_reactable_columns(elem.id, columns))
+        elem => Reactable.onStateChange(elem.id, state => truncate_reactable_columns(elem.id, keys))
     );
 }
 
-function truncate_reactable_columns(table_id, columns) {
-    insert_aqmap_links(); // ensure any visible entries setup for aqmap links formatted correctly
-    const tbl_cols = get_reactable_columns(table_id);
-    for (let j = 0; j < columns.length; j++) {
-        let header_label = columns[j];
-        let is_desired_col = tbl_cols.map(x => x === header_label);
-        is_desired_col.forEach((test, index) => { if (test) truncate_reactable_column(table_id, index) });
-    }
-};
-
-function get_reactable_columns(table_id) {
-    const table = document.getElementById(table_id);
-    const headers = table.querySelectorAll(".rt-tr-header .rt-th");
-    return [...headers].map(header => header.innerText);
-}
-
-function truncate_reactable_column(table_id, header_index) {
-    const table = document.getElementById(table_id);
-    const rows = table.querySelectorAll(".rt-tbody .rt-tr-group .rt-tr");
-    rows.forEach((row) => {
-        const cell = row.childNodes[header_index].querySelector(".rt-text-content");
-        cell.innerHTML = truncate_long_text(cell.innerHTML, cell.innerText);
+function truncate_reactable_columns(table_id, keys) {
+    insert_aqmap_links();
+    let table_data = Reactable.getState(table_id).data;
+    table_data.map((row) => {
+        console.log(row);
+        keys.forEach((key) => {
+            if(row[key]) row[key] = truncate_long_text(row[key]);
+        });
+        return row;
     });
-}
+    Reactable.setData(table_id, table_data);
+};
 
 function truncate_long_text(x, hover = x) {
     hover = hover.replace(/'/g, "&rsquo;").replace(/"/g, "&quot;");
@@ -37,6 +25,7 @@ function truncate_long_text(x, hover = x) {
 }
 
 // TODO: allow this to be controlled via R so not hardcoded names
-document.addEventListener('DOMContentLoaded', function() {
-  add_truncate_listeners(['Name', 'Region']);
-});
+document.addEventListener(
+    'DOMContentLoaded', 
+    () => add_truncate_listeners(['name', 'fcst_zone', 'nearest_community'])
+);
