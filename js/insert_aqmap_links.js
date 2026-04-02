a1e540713b21d47c8778a4fa8809c8ce96a60b99
Brayden Nilson <nilson.brayden@gmail.com>
2026-04-01 17:27:50 -0700

    replace the initial div data element instead of just the inner html and use &apos;

--- js/insert_aqmap_links.js
+++ js/insert_aqmap_links.js
@@ -4,11 +4,13 @@ function insert_aqmap_links() {
         const lng = element.getAttribute("data-lng");
         const lat = element.getAttribute("data-lat");
         const zoom = element.getAttribute("data-zoom") ?? 12;
-        element.innerHTML = make_aqmap_link(element.innerText, lng, lat, zoom);
+        const link = make_aqmap_link(element.innerText, lng, lat, zoom);
+        const new_elem_parent = new DOMParser().parseFromString(link, "text/xml")
+        element.replaceWith(new_elem_parent.firstChild);
     });
 }
 
 function make_aqmap_link(label, lng, lat, zoom = 12) {
-    let label_safe = label.replace(/'/g, "&rsquo;").replace(/"/g, "&quot;");
+    let label_safe = label.replace(/'/g, "&apos;").replace(/"/g, "&quot;");
     return `<a title="${label_safe}" href="https://aqmap.ca/aqmap/en/#${zoom}/${lat}/${lng}">${label}</a>`
 }
--- js/truncate_reactable_column.js
+++ js/truncate_reactable_column.js
@@ -33,7 +33,7 @@ function truncate_reactable_column(table_id, header_index) {
 function truncate_long_text(x, hover = x) {
     hover = hover.replace(/'/g, "&rsquo;").replace(/"/g, "&quot;");
     let styles = ["display:table; table-layout:fixed; width:100%", "overflow-x:hidden; text-overflow:ellipsis; white-space:nowrap"];
-    return `<div style="${styles[0]}"><p title="${hover}" style="${styles[1]}">${x}</p></div>`;
+    return `<div style="${styles[0]}"><div title="${hover}" style="${styles[1]}">${x}</div></div>`;
 }
 
 // TODO: allow this to be controlled via R so not hardcoded names
